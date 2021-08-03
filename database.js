4bd67b7cac5fc52f13b1f374364c8f56ce32a07b
AndreySFN <andreysfn@yanex.ru>
2021-08-03 16:36:57 +0300

    Разработка минимально необходимого функционала

--- database.js
+++ database.js
@@ -1,12 +1,18 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'AndreySFN',
+    host: '188.65.208.74',
+    port: '3306',
+    user: '185354_root',
     password: 'An25021999!'
 });
 
 db.connect();
-db.query(`USE rateform`);
+db.query(`USE 185354_rateform`);
+db.query("SET NAMES utf8");
+db.query("SET CHARACTER SET utf8");
+db.query("SET character_set_client = utf8");
+db.query("SET character_set_connection = utf8");
+db.query("SET character_set_results = utf8");
 
 module.exports = db;
--- routes/createFeedback.js
+++ routes/createFeedback.js
@@ -8,7 +8,7 @@ const {doSuccessResponse} = require("../responseUtils");
 const {doErrorResponse} = require("../responseUtils");
 
 router.post('/', upload.none(), function(req, res) {
-  db.query(`INSERT INTO feedbacks (name, feedback, rate, serverDateTime) values ("${req.body.name}", "${req.body.comment}", "${req.body.rate}", ${db.escape(new Date())});`,
+  db.query(`INSERT INTO feedbacks (name, feedback, rate, serverDateTime, user) values ("${req.body.name}", "${req.body.comment}", "${req.body.rate}", ${db.escape(new Date())}, "${req.body.employerName}");`,
       (err) => {
         if (err) {
             doErrorResponse(res, err);
